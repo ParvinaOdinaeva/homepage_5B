1c2c18fb38a48019029f44330cd770de26d770aa
Parvina Odinaeva <odinaeva.parvina@gmail.com>
2023-07-18 23:52:57 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+{
+  const calculateResult = (amount, currency) => {
+    const rateEUR = 4.5;
+    const rateUSD = 4.1;
+    const rateGBP = 5.2;
+
+    switch (currency) {
+      case "EUR":
+        return amount / rateEUR;
+
+      case "USD":
+        return amount / rateUSD;
+
+      case "GBP":
+        return amount / rateGBP;
+    }
+  }
+
+  const onFormSubmit = (event) => {
+      event.preventDefault();
+  
+      const amountElement = document.querySelector(".js-amountField");
+      const currencyElement = document.querySelector(".js-currencyField");
+      const resultElement = document.querySelector(".js-result");
+  
+      const amount = +amountElement.value;
+      const currency = currencyElement.value;
+  
+      const result = calculateResult(amount, currency);
+  
+      resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed(2)} ${currency}`;
+    }
+  
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", onFormSubmit); 
+  }
+
+  init();
+
+}
